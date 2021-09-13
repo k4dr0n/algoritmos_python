f9978cc5fa7a0ac09cc5fc0a436530249a1df003
k4dr0n <39683457+k4dr0n@users.noreply.github.com>
2021-09-13 11:00:42 -0300

    Create ex8.py

---
+++ ex8.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+sexo = raw_input("Digite F se for feminino e M se for masculino. ")
+
+if sexo == 'M':
+    print("Sexo e Masculino.")
+elif sexo == 'F':
+    print("Sexo e Feminino. ")
+else:
+    print("Informacao invalida.")
