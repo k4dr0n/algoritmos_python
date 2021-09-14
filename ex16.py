8450ea8a00db57ada107bf33db144ef3e9899f9d
k4dr0n <39683457+k4dr0n@users.noreply.github.com>
2021-09-14 01:53:17 -0300

    Update ex16.py

--- ex16.py
+++ ex16.py
@@ -1,49 +1,49 @@
+#Faça um programa que calcule as raízes de uma equação do segundo grau, 
+# na forma ax2+bx+c, cuja solução é x=−b±Δ−−√2⋅a, com Δ=b2−4⋅a⋅c. 
+#O programa deverá pedir os valores de a, b e c e fazer as consistências, informando ao usuário nas seguintes situações:
+#Se o usuário informar o valor de A igual a zero, a equação não é do segundo grau 
+#e o programa não deve fazer pedir os demais valores, sendo encerrado;
+#Se o delta calculado for negativo, a equação não possui raízes reais. Informe ao usuário e encerre o programa;
+#Se o delta calculado for igual a zero a equação possui apenas uma raiz real, informe-a ao usuário;
+#Se o delta for positivo, a equação possui duas raízes reais, informe-as ao usuário.
 
-#Importamos la librera math para usar el metodo sqrt
-import math
 
-#Pedimos la entrade de los tres valores de la ecuacio, a = cociente del termino cuadratico, b = cociente de termino lineal y c = termino independiente
-a = float(input("Ingrese el valor de A: "))
-b = float(input("Ingrese el valor de B: "))
-c = float(input("Ingrese el valor de C: "))
+import math  #para utilizar a função sqrt
 
-#Como nos dice la formula, restamos el valor de b elevado a la 2, con el valode de 4 * a * c
-x = (b**2)-(4*a*c)
+# Checar a validade dos dados de entrada
+def check_grau(a):
+    if a==0:
+        return False
+    else:
+        return True
 
-#Si x es menor que 0, no tendra una solucion dentro de las reglas de los numeros reales
-if x < 0:
-    print("Solucion solo en numeros complejos")
-elif x == 0:
-    x1 = (-b) / (2*a)
-    print("%.2f" % x1)
+def raizes(a,b,c):    
+    delta = (b*b)-(4*a*c)
+    if delta < 0:
+        print("Delta negativo. Equacao nao possui raizes reais!")
+    elif delta == 0:
+        raiz = -b / (2*a)
+        print("Delta = 0. Equacao possui uma raiz real igual a %.2f" % raiz)
+    else:
+        delta_2 = math.sqrt(delta)
+        raiz_1 = (-b + delta_2) / (2*a)
+        raiz_2 = (-b - delta_2) / (2*a)
+        print("Delta > 0. Equacao possui duas raizes reais." )
+        print("Raiz 1 igual a %.2f" % raiz_1)
+        print("Raiz 2 igual a %.2f" % raiz_2)
+        
+
+
+#Dados de entrada
+print("Considerando ax^2 + bx + c = 0, entre com os seguintes coeficientes")
+coef_a = float(input("Valor numerico de a: "))
+if check_grau(coef_a):
+    print("Equacao de 2o grau. Entre com os demais valores.")
+    coef_b = float(input("Valor numerico de b: "))
+    coef_c = float(input("Valor numerico de c: "))
+    raizes(coef_a,coef_b,coef_c)
 else:
-	#Luego sumamos o restamos (nos dara 2 valores diferentes, pero ambos igualaran la ecuacion)
-	#sumamos el valor de -b a la raiz cuadrada de x y luego lo dividimos entre 2 * a
-    x1 = (-b + math.sqrt(x)) / (2*a)
-    x2 = (-b - math.sqrt(x)) / (2*a)
-
-    #Mostramos ambos valores
-    print("X1: ", x1)
-    print("X2: ", x2)
+    print("Valor de a não pode ser igual a zero!")
 
 
-
-a = float(input('Entre com o valor de a: '))
-b = float(input('Entre com o valor de b: '))
-c = float(input('Entre com o valor de c: '))
-
-delta = (b ** 2) - 4 * a * c
-
-print("\n**************************\n")
-
-if a == 0:
-    print("O valor de a, deve ser diferente de 0")
-elif delta < 0:
-    print("Sem raízes reais")
-else:
-    x1 = (-b + delta ** (1 / 2)) / (2 * a)
-    x2 = (-b - delta ** (1 / 2)) / (2 * a)
-
-    print("x1: {}, x2: {}".format(x1, x2))
-    
-  
+        
