42872b60d2e1962f9757aff7f8b80ea6d27ee12e
k4dr0n <39683457+k4dr0n@users.noreply.github.com>
2021-09-14 02:06:37 -0300

    Update ex17.py

--- ex17.py
+++ ex17.py
@@ -1,16 +1,25 @@
-ano = int(input('Ano: '))
-if (ano%4==0 and ano%100!=0) or (ano%400==0):
-    print('Bissexto')
-else:
-    print('Não é bissexto')
+#Faça um programa que peça um número correspondente a um determinado ano 
+#e em seguida informe se este ano é ou não bissexto. 
+#Um ano é bissexto se ele é múltiplo de quatro. 
+#No entanto anos múltiplos de 100 que não são múltiplos de 400 não são bissextos. 
+#Então 1995 não é bissexto, 2012 é bissexto, 1900 não é bissexto e 2000 é bissexto.
+
+def check_bissexto(ano):
+    if (ano%4==0 and ano%100!=0) or (ano%400==0):
+        print("Eh Bissexto de acordo com o algoritmo que eu implementei.")
+    else:
+        print("Não eh bissexto de acordo com o algoritmo que eu implementei.")
+    
+    
+    
+ano_inp = int(input('Ano: '))
+
+check_bissexto(ano_inp)
     
 from calendar import isleap
 
-ano = 2021
 
-if isleap(ano):
-    print('É bissexto')
+if isleap(ano_inp):
+    print("Continua bissexto de acordo com o algoritmo do python.")
 else:
-    print('Não é bissexto')
-    
-    
+    print("Continua nao sendo bissexto de acordo com o algoritmo do python.")
