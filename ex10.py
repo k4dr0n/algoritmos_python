97debe04fbf96eb82988a054bdc85a9aad830f82
k4dr0n <39683457+k4dr0n@users.noreply.github.com>
2021-09-14 00:56:38 -0300

    Update ex10.py

--- ex10.py
+++ ex10.py
@@ -1,66 +1,62 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+#Faça um programa para o cálculo de uma folha de pagamento, 
+#sabendo que os descontos são dados na lista abaixo. 
+#O Salário Líquido corresponde ao Salário Bruto menos os descontos (imposto e sindicato). 
+#O programa deverá pedir ao usuário o valor da sua hora e a quantidade de horas trabalhadas no mês, 
+#e deve imprimir todos os cálculos realizados. O desconto do IR é calculado conforme a tabela em seguida.
 
-#Faça um programa para o cálculo de uma folha de pagamento,
-#sabendo que os descontos são do Imposto de Renda,
-# que depende do salário bruto (conforme tabela abaixo) e 3% para
-#o Sindicato e que o FGTS corresponde a 11% do Salário Bruto,
-#mas não é descontado (é a empresa que deposita).
-#O Salário Líquido corresponde ao Salário Bruto menos os descontos. 
-#O programa deverá pedir ao usuário o valor da sua hora e a quantidade de horas trabalhadas no mês.
-# Desconto do IR:
+# Imposto de Renda, que depende do salário bruto (conforme tabela abaixo):
 # Salário Bruto até 900 (inclusive) - isento
 # Salário Bruto até 1500 (inclusive) - desconto de 5%
 # Salário Bruto até 2500 (inclusive) - desconto de 10%
-# Salário Bruto acima de 2500 - desconto de 20% Imprima na tela as informações, dispostas conforme o exemplo abaixo.
-#No exemplo o valor da hora é 5 e a quantidade de hora é 220. 
+# Salário Bruto acima de 2500 - desconto de 20%
 
-hora = input("Informa o valor recebido por hora trabalhada. ")
-qhora = input("Informe a quantidade de horas trabalhadas no mes. ")
+# 3% do salário bruto para o Sindicato;
 
-salariob = hora * qhora
-fgts = (salariob * 11) / 100
-sind = (salariob * 3) / 100
+# O FGTS corresponde a 11% do Salário Bruto, mas não é descontado (é a empresa que deposita).
 
-if salariob <= 900:
-    salariol = salariob - sind
 
-elif salariob > 900 and salariob <=  1500:
-    ir = (salariob * 5) / 100
-    salariol = salariob - ir - sind
- 
-elif salariob > 1500 and salariob <= 2500:
-    ir = (salariob * 10) / 100
-    salariol = salariob - ir - sind
-
-else:
-    ir = (salariob * 20) / 100
-    salariol = salariob - ir -sind
     
-print("Seu salario bruto e : %7.2f" % salariob)
-print("O valor de seu FGTS e de: %7.2f" % fgts)
-print("O sindicato vai te levar: %7.2f" % sind)
-print("Seu salario liquido e de: %7.2f" % salariol) 
-
-
-
+def salario(horas_trabalhadas, valor_hora=10):
+  return horas_trabalhadas * valor_hora
 
 def relatorio_salario(horas_trabalhadas, valor_hora):
-  salario_bruto = salario(horas_trabalhadas, valor_hora)
+    salario_bruto = salario(horas_trabalhadas, valor_hora)
 
-  irpf = 0.11 * salario_bruto
-  inss = 0.08 * salario_bruto
-  sind = 0.05 * salario_bruto
+    fgts = 0.11 * salario_bruto
+    sind = 0.03 * salario_bruto
 
-  salario_liquido = salario_bruto - irpf - inss - sind
+    salario_liquido = salario_bruto  - sind
 
-  print("Salário por hora trabalhada:", valor_hora)
-  print("Número de horas trabalhadas no mês:", horas_trabalhadas)
-  print("------------------------------------------------")
-  print("Salário bruto:", salario_bruto)
-  print("(-) Imposto de Renda:", irpf)
-  print("(-) INSS:", inss)
-  print("(-) Sindicato:", sind)
-  print("(=) Salário Líquido:", salario_liquido)
+    if salario_bruto > 900 and salario_bruto <=  1500:
+        irpf = 0.05 * salario_bruto
+        salario_liquido = salario_bruto - irpf
 
-relatorio_salario(20, 10.5)
+    elif salario_bruto > 1500 and salario_bruto <= 2500:
+        irpf = 0.10 * salario_bruto
+        salario_liquido = salario_bruto - irpf
+
+    else:
+        irpf = 0.20* salario_bruto
+        salario_liquido = salario_bruto - irpf
+
+
+
+
+    print("Salário por hora trabalhada: R$ %5.2f" % valor_hora)
+    print("Número de horas trabalhadas no mês: %4d" % horas_trabalhadas)
+    print("------------------------------------------------")
+    print("Seu salario bruto e : R$ %7.2f" % salario_bruto)
+    print("O valor de seu FGTS e de: R$ %6.2f" % fgts)
+    print("O sindicato vai te levar: R$ %5.2f" % sind)
+    print("Seu salario liquido e de: R$ %7.2f" % salario_liquido) 
+    
+    if salario_bruto <= 900: 
+        print("Parabéns (ou não), você está isendo de Imposto!")
+    else:
+        print("Você foi extorquido em: R$ %6.2f de Imposto!" % irpf) 
+
+
+hora = float(input("Informa o valor recebido por hora trabalhada em R$. "))
+n_horas = float(input("Informe a quantidade de horas trabalhadas no mes. "))
+
+relatorio_salario(n_horas, hora)
